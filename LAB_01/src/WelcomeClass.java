da1cf124fb213d95b0bf8044a9287c47427d0e69
Ruslan1Shkraba <ruslan.shkraba.ki.2020@lpnu.ua>
2022-08-24 19:46:21 +0300

    Signed-off-by: Ruslan1Shkraba <ruslan.shkraba.ki.2020@lpnu.ua>

--- LAB_01/src/WelcomeClass.java
+++
@@ -1,7 +0,0 @@
-
-public class WelcomeClass {
-
-	public static void main(String[] args) {
-		System.out.println("Ruslan Shkraba, KI-34");
-	}
-}
---
+++ Lab_01/src/HelloJava.java
@@ -0,0 +1,10 @@
+
+public class HelloJava {
+
+	public static void main(String[] args)
+	{
+	String message = new String();
+	message = "Ruslan Shkraba, KI-34";
+	System.out.println(message);
+	}
+	}
