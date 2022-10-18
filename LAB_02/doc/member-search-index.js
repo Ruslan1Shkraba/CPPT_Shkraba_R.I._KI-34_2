e091a99593333dacbdd8ebc4b72330077d091f8e
Ruslan1Shkraba <ruslan.shkraba.ki.2020@lpnu.ua>
2022-10-18 15:11:21 +0300

    generated documentation in Javadoc

---
+++ LAB_02/doc/member-search-index.js
@@ -0,0 +1,1 @@
+memberSearchIndex = [{"p":"<Unnamed>","c":"Lab2_Shkraba_KI34","l":"Lab2_Shkraba_KI34()","u":"%3Cinit%3E()"},{"p":"<Unnamed>","c":"Lab2_Shkraba_KI34","l":"main(String[])","u":"main(java.lang.String[])"}];updateSearchResults();
---
+++ LAB_02/doc/type-search-index.js
@@ -0,0 +1,1 @@
+typeSearchIndex = [{"l":"All Classes and Interfaces","u":"allclasses-index.html"},{"p":"<Unnamed>","l":"Lab2_Shkraba_KI34"}];updateSearchResults();
